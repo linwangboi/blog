83299562c1cf4e3a373d57661f09c16eb45fdd24
Sadie <an5dy90@gmail.com>
2026-05-18 19:38:33 +0800

    feat: registration, login

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
@@ -22,12 +23,12 @@ from blogs import views as BlogsView
 
 from . import views
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('category/', include('blogs.urls')),
-    path('<slug:slug>/', BlogsView.blogs, name='blogs'),
-    path('blogs/search/', BlogsView.search, name='search'),
-
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("category/", include("blogs.urls")),
+    path("blogs/<slug:slug>/", BlogsView.blogs, name="blogs"),
+    path("blogs/search/", BlogsView.search, name="search"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="login"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,19 +1,57 @@
-
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from assignments.models import About
 from blogs.models import Blog, Category
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def home(request):
-    featured_posts = Blog.objects.filter(is_featured=True).order_by('updated_at')
-    posts = Blog.objects.filter(is_featured=False, status='Published')
+    featured_posts = Blog.objects.filter(is_featured=True).order_by("updated_at")
+    posts = Blog.objects.filter(is_featured=False, status="Published")
     try:
         about = About.objects.get()
     except:
         about = None
-    return render(request, 'home.html', {
-        'featured_posts': featured_posts,
-        'posts': posts,
-        'about': about,
-    })
+    return render(
+        request,
+        "home.html",
+        {
+            "featured_posts": featured_posts,
+            "posts": posts,
+            "about": about,
+        },
+    )
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        else:
+            print(form.errors)
+    else:
+        form = RegistrationForm()
+    return render(
+        request,
+        "register.html",
+        {
+            "form": form,
+        },
+    )
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth.login(request, form.get_user())  # 👈 One line, one source of truth
+            return redirect("home")
+        print(form.errors)  # Move outside if-block
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
